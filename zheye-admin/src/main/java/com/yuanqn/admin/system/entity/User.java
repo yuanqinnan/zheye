ae1513d7d31450f8de2c67fe530ff32ccc6fafda
yuanqinnan <1546022688@qq.com>
2019-07-13 17:03:03 +0800

    clear

---
+++ zheye-admin/src/main/java/com/yuanqn/admin/system/entity/User.java
@@ -0,0 +1,17 @@
+package com.yuanqn.admin.system.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @author:yuanqinnan
+ * @date: 2019/7/13 16:28
+ */
+
+@Data
+@TableName("t_admin_user")
+public class User {
+
+
+
+}
